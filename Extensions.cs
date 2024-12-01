9ef4c20b3dc37670ebccf7f49bc283cf41d7dc8e
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-11-30 17:44:42 -0800

    Extensions class

---
+++ Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderwaterGame
+{
+    public static class Extensions
+    {
+        /// <summary>Convert a boolean value to a float</summary>
+        /// <param name="input">bool to convert</param>
+        /// <returns>1f for true, 0f for false</returns>
+        public static float ToFloat(this bool input)
+        {
+            return input ? 1f : 0f;
+        }
+    }
+}
