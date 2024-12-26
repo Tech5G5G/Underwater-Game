f4c47b938924589c56ab90ae2cf0d4cf3cff5369
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-12-25 22:18:46 -0800

    Spawn 10 fish instead of infinite

--- scripts/Map.cs
+++ scripts/Map.cs
@@ -8,21 +8,13 @@ public partial class Map : Node3D
 
 	public override void _Ready()
 	{
-		var timer = new Timer() { OneShot = false };
-		AddChild(timer);
-		timer.Timeout += () =>
+        Random ran = new();
+        for (int i = 10; i > 0; i--)
 		{
-			var fish = fishScene.Instantiate<Fish>();
-			Random ran = new();
-
-			fish.Position = new Vector3(ran.Next(0, 20), ran.Next(1, 20), ran.Next(0, 20));
-			AddChild(fish);
-		};
-		timer.Start(2);
-
-		//Temp code to disable fog
-		var environment = GetNode<WorldEnvironment>("WorldEnvironment");
-		environment.Environment.VolumetricFogEnabled = false;
+            var fish = fishScene.Instantiate<Fish>();
+            fish.Position = new Vector3(ran.Next(0, 100), ran.Next(1, 100), ran.Next(0, 100));
+            AddChild(fish);
+        }
 	}
 
 	public override void _Process(double delta)
