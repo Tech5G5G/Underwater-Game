dedb9cdb90774c1f9f20d91e9ec75c412cb17a93
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-12-25 22:18:13 -0800

    Fix LookFollow method

--- scripts/Fish.cs
+++ scripts/Fish.cs
@@ -10,38 +10,33 @@ public partial class Fish : RigidBody3D
     public Vector3 forwardAxis = new(0, 0, -1);
     public int forwardAxisCooldown = 0;
 
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
         playerWindscreen = GetParent().GetNode<Node3D>("Jet/Leveling");
 
-		GravityScale = 0;
-		Mass = 0.3f;
+        GravityScale = 0;
+        Mass = 0.3f;
 
         ContactMonitor = true;
         MaxContactsReported = 1;
-	}
+    }
 
     private void LookFollow(PhysicsDirectBodyState3D state, Transform3D currentTransform, Vector3 targetPosition)
     {
-        Vector3 forwardLocalAxis = new(1, 0, 0);
-        Vector3 forwardDir = (currentTransform.Basis * forwardLocalAxis).Normalized();
+        Vector3 forwardDir = (currentTransform.Basis * forwardAxis).Normalized();
         Vector3 targetDir = (targetPosition - currentTransform.Origin).Normalized();
         float localSpeed = Mathf.Clamp(_speed, 0.0f, Mathf.Acos(forwardDir.Dot(targetDir)));
 
         AngularVelocity = forwardDir.Cross(targetDir) * localSpeed / state.Step;
-
-        // if (forwardDir.Dot(targetDir) > 1e-4)
-        // {
-        // }
     }
 
     public override void _IntegrateForces(PhysicsDirectBodyState3D state)
     {
-		LookFollow(state, GlobalTransform, jet.GlobalTransform.Origin);
-   	}
+        LookFollow(state, GlobalTransform, playerWindscreen.GlobalTransform.Origin);
+    }
 
     public override void _Process(double delta)
-	{
+    {
         if (forwardAxisCooldown > 0)
         {
             forwardAxisCooldown--;
@@ -60,5 +55,5 @@ public partial class Fish : RigidBody3D
         }
 
         LinearVelocity = GlobalTransform.Basis * new Vector3(0, 0, -10);
-	}
+    }
 }
