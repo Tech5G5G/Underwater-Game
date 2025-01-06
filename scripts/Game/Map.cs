bc2fa73356b0634242a2f0ed3a710b5d72768d8b
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-01-06 06:41:43 -0800

    Add spawning of different types of fish

--- scripts/Game/Map.cs
+++ scripts/Game/Map.cs
@@ -3,14 +3,24 @@ using System;
 
 public partial class Map : Node3D
 {
-	private PackedScene fishScene = GD.Load<PackedScene>("res://scenes/Fish.tscn");
+	private PackedScene blueFishScene = GD.Load<PackedScene>("res://fishes/BlueFish.tscn");
+    private PackedScene greenFishScene = GD.Load<PackedScene>("res://fishes/GreenFish.tscn");
+    private PackedScene epikFishScene = GD.Load<PackedScene>("res://fishes/EpikFish.tscn");
+
+    private PackedScene settingsWindow = GD.Load<PackedScene>("res://scenes/SettingsWindow.tscn");
 
 	public override void _Ready()
 	{
         Random ran = new();
         for (int i = 20; i > 0; i--)
 		{
-            var fish = fishScene.Instantiate<Fish>();
+            Fish fish = ran.Next(1, 8) switch
+            {
+                <= 3 => blueFishScene.Instantiate<Fish>(),
+                <= 6 => greenFishScene.Instantiate<Fish>(),
+                7 => epikFishScene.Instantiate<Fish>(),
+                _ => throw new IndexOutOfRangeException("Number is not between 1 and 8.")
+            };
             fish.Position = new Vector3(ran.Next(0, 100), ran.Next(1, 100), ran.Next(0, 100));
             AddChild(fish);
         }
