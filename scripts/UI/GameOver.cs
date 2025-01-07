cc059c99ca52cff8b85e020b032a96be07a582c4
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-01-07 10:52:39 -0800

    Added game over screen

---
+++ scripts/UI/GameOver.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class GameOver : Control
+{
+	public Button Resume;
+	public Button Menu;
+
+	public override void _Ready()
+	{
+		var tree = GetTree();
+		tree.Paused = true;
+
+		(Resume = GetNode<Button>("Control/Respawn")).ButtonUp += () =>
+		{ 
+            tree.Paused = false;
+			tree.ChangeSceneToFile("res://scenes/map.tscn");
+		};
+		(Menu = GetNode<Button>("Control/Menu")).ButtonUp += () =>
+		{
+            player.HealthPower = null;
+            tree.Paused = false;
+			tree.ChangeSceneToFile("res://scenes/MainMenu.tscn");
+		};
+
+        Input.MouseMode = Input.MouseModeEnum.Visible;
+		this.ProcessMode = ProcessModeEnum.Always;
+	}
+}
