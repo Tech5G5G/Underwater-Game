b1b9797a181621077a58a49248c6df6dcc00b9c7
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-01-06 18:00:39 -0800

    Removed open settings window from Map.cs

--- scripts/Game/Map.cs
+++ scripts/Game/Map.cs
@@ -7,15 +7,8 @@ public partial class Map : Node3D
     private PackedScene greenFishScene = GD.Load<PackedScene>("res://fishes/GreenFish.tscn");
     private PackedScene epikFishScene = GD.Load<PackedScene>("res://fishes/EpikFish.tscn");
 
-    private PackedScene settingsWindow = GD.Load<PackedScene>("res://scenes/SettingsWindow.tscn");
-
     public override void _Ready()
 	{
-        //Activate window like this
-        //var window = settingsWindow.Instantiate<Window>();
-        //AddChild(window);
-        //window.Show();
-
         Random ran = new();
         for (int i = 20; i > 0; i--)
 		{
@@ -24,7 +17,7 @@ public partial class Map : Node3D
                 <= 3 => blueFishScene.Instantiate<Fish>(),
                 <= 6 => greenFishScene.Instantiate<Fish>(),
                 7 => epikFishScene.Instantiate<Fish>(),
-                _ => throw new IndexOutOfRangeException("Number is not between 1 and 8.")
+                _ => throw new IndexOutOfRangeException("Number is not between 1 and 7.")
             };
             fish.Position = new Vector3(ran.Next(0, 100), ran.Next(1, 100), ran.Next(0, 100));
             AddChild(fish);
